909ed7b4caf083bac526893fc7310876d0f1db5a
Axel <buobaxel@gmail.com>
2021-11-24 10:12:11 +0100

    admin link added in footer

--- src/app/view/template/default.php
+++ src/app/view/template/default.php
@@ -115,9 +115,9 @@
         <?= $content; ?>
     </main>
     <!-- FOOTER -->
-    <footer class="footer mt-auto py-2 bg-warning">
-        <div class="container">
-            <ul class="nav flex-row justify-content-center">
+    <footer class="footer mt-auto py-3 bg-warning">
+        <div class="text-center container">
+            <ul class="nav flex-row justify-content-center mb-3">
                 <li class="nav-item mx-3">
                     <a href="<?= ($config->github) ?: $config->github; ?>" class="nav-link link-dark px-2">
                         <i class="fab fa-github fa-2x"></i>
@@ -134,6 +134,7 @@
                     </a>
                 </li>
             </ul>
+            <a class="link-dark my-5" href="/portofolio/admin/">Administration</a>
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
