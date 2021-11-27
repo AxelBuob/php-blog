f414c0c4bb5a07f4a0c720f2b36088758a1d410c
Axel <buobaxel@gmail.com>
2021-11-27 14:39:32 +0100

    fix #55  feat: sitemap

--- public/index.php
+++ public/index.php
@@ -2,12 +2,14 @@
 
 use \App\Factory;
 use \App\Router;
-use Intervention\Image\ImageManager;
+use samdark\sitemap\Sitemap;
+use samdark\sitemap\Index;
 
 define('ROOT', dirname(__DIR__));
 define('BASE_DIR', 'portofolio');
 define('UPLOAD_DIR', ROOT . '/public/uploads/');
 define('UPLOAD_PATH', '/portofolio/public/uploads/');
+define('HOST', 'http://localhost/portofolio/');
 
 define('KB', 1024);
 define('MB', 1048576);
@@ -18,10 +20,39 @@ require '../vendor/autoload.php';
 require ROOT . '/src/app/Factory.php';
 Factory::autoloading();
 
+$sitemap = new Sitemap(__DIR__ . '/sitemap.xml');
+// Page
+$sitemap->addItem(HOST . '/page/contact',);
+$sitemap->addItem(HOST . '/page/resume');
+// USER
+$sitemap->addItem(HOST . '/user/signin');
+$sitemap->addItem(HOST . '/user/signout');
+$sitemap->addItem(HOST . '/user/forget');
+$sitemap->addItem(HOST . '/user/show/?id=1');
+
+
+// POST
+$sitemap->addItem(HOST . '/post/show/?id=3');
+$sitemap->addItem(HOST . '/post/show/?id=15');
+$sitemap->addItem(HOST . '/post/show/?id=16');
+
+// POST
+$sitemap->addItem(HOST . '/post/category/?id=1');
+$sitemap->addItem(HOST . '/post/category/?id=2');
+$sitemap->addItem(HOST . '/post/category/?id=3');
+
+$sitemap->write();
+$sitemapFileUrls = $sitemap->getSitemapUrls('http://localhost/portofolio/');
+
+
 
 
 $router = new Router(BASE_DIR);
 $router->init();
+
+
+
+
 
 
 
